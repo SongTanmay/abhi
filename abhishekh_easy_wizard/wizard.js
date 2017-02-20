1a84538ee40e0402b5ec96be9323d26dda87ab9a
Tanmay Songade <tsongade@gmail.com>
2017-02-19 22:38:31 -0500

    Added practice and diagnosis

--- abhishekh_easy_wizard/wizard.js
+++ abhishekh_easy_wizard/wizard.js
@@ -6,48 +6,128 @@ $(function() {
     buttonsClass: 'btn',
     submitButtonClass: 'btn btn-info',
     before: function(wizardObj, currentStepObj, nextStepObj) {
-      //if($( "input[name=gender1]:checked" ).val() === 'No'){
-        //alert('Your answer is wrong. Try again');
-      //} else if($( "input[name=gender1]:checked" ).val() === 'Yes'){
-      //  $("#success-alert").alert();
-      //  $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
-      //    $("#success-alert").slideUp(500);
-      //  });
-        //sleep(3000);
-    //  }
+      //practice1
+      if($( "input[name=practice1]:checked" ).val() === 'bad'){
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+          $("#success-alert").slideUp(500);
+        });
+        $( "input[name=practice1]:checked" ).prop('checked',false)
+      }
+      //practice2
+      if($( "input[name=practice2]:checked" ).val() === 'cancel'){
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+          $("#success-alert").slideUp(500);
+        });
+        $( "input[name=practice2]:checked" ).prop('checked',false)
+      }
+      //practice3
+      var expected = ["thin", "extra", "touch"];
+      var checkedValues = $('input:checkbox:checked').map(function() {
+                              return this.value;
+                          }).get();
+      if(checkedValues.toLocaleString() === expected.toLocaleString()){
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+          $("#success-alert").slideUp(500);
+        });
+        $('input:checkbox').removeAttr('checked');
+      }
+      //practice4
+      if($( "input[name=practice4]:checked" ).val() === 'send'){
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+          $("#success-alert").slideUp(500);
+        });
+        $( "input[name=practice4]:checked" ).prop('checked',false)
+      }
     },
     after: function(wizardObj, currentStepObj, nextStepObj) {
-      if($( "input[name=gender1]:checked" ).val() === 'No'){
-        $("#info").css('display','block');
-        $( "input[name=gender1]:checked" ).prop('checked',false)
+      //practice1
+      if($( "input[name=practice1]:checked" ).val() === 'good'){
+        $("#practice1_info").css('display','block');
+        $( "input[name=practice1]:checked" ).prop('checked',false)
         $('#practice').easyWizard('prevStep');
-     } else if($( "input[name=gender1]:checked" ).val() === 'Yes'){
-        $( "input[name=gender1]:checked" ).prop('checked',false)
       }
-      if($( "input[name=think]:checked" ).val() === 'First'){
+      //practice2
+      if($( "input[name=practice2]:checked" ).val() === 'save' || $( "input[name=practice2]:checked" ).val() === 'cancel_save' || $( "input[name=practice2]:checked" ).val() === 'nocancel_nosave'){
+        $("#practice2_info").css('display','block');
+        $( "input[name=practice2]:checked" ).prop('checked',false)
+        $('#practice').easyWizard('prevStep');
+      }
+      //practice3
+      var expected = ["thin", "extra", "touch"];
+      var checkedValues = $('input:checkbox:checked').map(function() {
+                              return this.value;
+                          }).get();
+      if(checkedValues.toLocaleString() !== expected.toLocaleString() && checkedValues.length > 0){
+       $("#practice3_info").css('display','block');
+       $('input:checkbox').removeAttr('checked');
+       $('#practice').easyWizard('prevStep');
+      }
+      //diagno practice4
+      if($( "input[name=practice4]:checked" ).val() === 'cancel_violates' || $( "input[name=practice4]:checked" ).val() === 'cancel_send' || $( "input[name=practice4]:checked" ).val() === 'nocancel_nosend'){
         $("#diagnosis").css('display','block');
         $("#practice").css('display','none');
-        $( "input[name=think]:checked" ).prop('checked',false);
+        $( "input[name=practice4]:checked" ).prop('checked',false);
         $('#practice').easyWizard('prevStep');
+        $("#diagnosis_header").css('display','block');
       }
     }
   });
-  $('#diagnosis').easyWizard({
-      buttonsClass: 'btn',
-      submitButtonClass: 'btn',
-      submitButtonText: 'Go Back To Practice',
-      before: function(wizardObj, currentStepObj, nextStepObj) {
 
+  $('#diagnosis').easyWizard({
+      showSteps: false,
+      submitButton: false,
+      buttonsClass: 'btn',
+      //submitButtonClass: 'btn',
+      //submitButtonText: 'Go Back To Practice',
+      before: function(wizardObj, currentStepObj, nextStepObj) {
+        //diagnosis1
+        if($( "input[name=diagnosis1]:checked" ).val() === 'camel_upper'){
+          $("#success-alert").alert();
+          $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+            $("#success-alert").slideUp(500);
+          });
+          $( "input[name=diagnosis1]:checked" ).prop('checked',false)
+        }
+        //diagnosis2
+        if($( "input[name=diagnosis2]:checked" ).val() === 'camel'){
+          $("#success-alert").alert();
+          $("#success-alert").fadeTo(5000, 500).slideUp(500, function(){
+            $("#success-alert").slideUp(500);
+          });
+          $( "input[name=diagnosis2]:checked" ).prop('checked',false)
+        }
       },
       after: function(wizardObj, currentStepObj, nextStepObj) {
-
-      },
-      beforeSubmit: function(wizardObj) {
-        if($( "input[name=diagnosis]:checked" ).val() === 'Camel'){
+        //diagnosis1
+        if($( "input[name=diagnosis1]:checked" ).val() === 'camel' || $( "input[name=diagnosis1]:checked" ).val() === 'lower_upper' || $( "input[name=diagnosis1]:checked" ).val() === 'none_of_the_above'){
+          $( "input[name=diagnosis1]:checked" ).prop('checked',false)
+          $('#diagnosis').easyWizard('prevStep');
+        }
+        //diagnosis2
+        if($( "input[name=diagnosis2]:checked" ).val() === 'upper'){
+          $( "input[name=diagnosis2]:checked" ).prop('checked',false)
+          $('#diagnosis').easyWizard('prevStep');
+        }
+        //diagnosis3
+        if($( "input[name=diagnosis3]:checked" ).val() === 'lower'){
+          $( "input[name=diagnosis3]:checked" ).prop('checked',false)
+          $('#diagnosis').easyWizard('prevStep');
+        }
+        //diagnosis3
+        if($( "input[name=diagnosis3]:checked" ).val() === 'upper'){
+          $("#diagnosis").easyWizard('goToStep', 1);
           $("#diagnosis").css('display','none');
           $("#practice").css('display','block');
-          $( "input[name=diagnosis]:checked" ).prop('checked',false)
+          $("#diagnosis_header").css('display','none');
+          $( "input[name=diagnosis3]:checked" ).prop('checked',false);
         }
+      },
+      beforeSubmit: function(wizardObj) {
+
       }
   });
   $("#diagnosis").css('display','none');
